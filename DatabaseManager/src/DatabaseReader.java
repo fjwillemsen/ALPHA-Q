afe1b0b19e23b28d42ab1ace4041b76a5e30cde8
Floris-Jan Willemsen <fjwillemsen@icloud.com>
2016-10-29 08:22:35 +0200

    Re-added the DatabaseReader to DatabaseManager after it was lost in the merge.

---
+++ DatabaseManager/src/DatabaseReader.java
@@ -0,0 +1,23 @@
+/**
+ * Created by floris-jan on 23-06-16.
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class DatabaseReader {
+    static final String DB_URL = "jdbc:sqlite:libs/datasource.db";
+    Connection connection;
+    Statement statement;
+
+    public DatabaseReader() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection(DB_URL);
+            statement = connection.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
