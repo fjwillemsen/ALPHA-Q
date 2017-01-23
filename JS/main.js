abcafe6cbac321e379a1b02418a969994038e5e1
Floris-Jan Willemsen <fjwillemsen@icloud.com>
2017-01-24 00:16:21 +0100

    Attempt

--- JS/main.js
+++ JS/main.js
@@ -314,7 +314,7 @@ function carViewed() {
     var query = "MERGE (n:Statistic { day: " + new Date().getDate() + ", month: " + (new Date().getMonth() + 1) + ", year: " + (new Date().getYear() + 1900) + "}) ON CREATE SET n.carsviewed = 1, n.carsbought = 0 ON MATCH SET n.carsviewed = n.carsviewed + 1;";
     console.log(query);
     db.cypher({ query: query }, function(err, results) {
-        return 'hello'
+        ...
     });
 }
 
