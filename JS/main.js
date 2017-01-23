dde910b07a710a05eab5cce46bd6b53f2cab0c75
Floris-Jan Willemsen <fjwillemsen@icloud.com>
2017-01-24 00:18:42 +0100

    Attempt

--- JS/main.js
+++ JS/main.js
@@ -313,9 +313,7 @@ function numberOfCarsBought(req, res, next) {
 function carViewed() {
     var query = "MERGE (n:Statistic { day: " + new Date().getDate() + ", month: " + (new Date().getMonth() + 1) + ", year: " + (new Date().getYear() + 1900) + "}) ON CREATE SET n.carsviewed = 1, n.carsbought = 0 ON MATCH SET n.carsviewed = n.carsviewed + 1;";
     console.log(query);
-    db.cypher({ query: query }, function(err, results) {
-        
-    });
+    db.cypher({ query: query }, null);
 }
 
 function carBought() {
