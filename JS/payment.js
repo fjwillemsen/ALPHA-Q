9f5aa4106f0bbac7422f7d2121d976721bc8f56a
Edgar Buyten <edgarbuyten@gmail.com>
2017-02-01 16:24:30 +0100

    Whoopwhoop. Final commit. Few bugfixes.

--- JS/payment.js
+++ JS/payment.js
@@ -3,26 +3,3 @@ function cardValidation() {
     $.get('http://' + ip + ':' + port + '/stats/carBought'); // Adds one to the number of cars bought today in the database
 }
 
-//        var kaart = getFormObjects('#kreka')
-//        if(kaart['number'] == "" || kaart['number'] ==  null){
-//            var al = document.getElementById("kaartnummer")
-//            al.innerHTML = "Please fill in your creditcard number"
-//
-//        }
-//        if(kaart['name'] == ""){
-//            var al = document.getElementById("voornaam")
-//            al.innerHTML = "Please fill in your first name"
-//        }
-//        if(kaart['expiry'] == ""){
-//            var al = document.getElementById("verloop")
-//            al.innerHTML = "Please fill in your expiry date"
-//        }
-//        if(kaart['cvc'] == ""){
-//            var al = document.getElementById("codecode")
-//            al.innerHTML = "Please fill in your CVC number"
-////        }
-////        else{
-//              setContentTo('afterPayment.html');
-////        }
-//
-//    }
